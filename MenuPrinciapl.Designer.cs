0fa708ee3ff0d3fc06f3594efd4b4d076a98a963
Valentina <rodriguezvalentinae@gmail.com>
2022-10-13 14:02:47 -0300

    Cambio de Valen

--- MenuPrinciapl.Designer.cs
+++ MenuPrinciapl.Designer.cs
@@ -31,12 +31,13 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(143, 57);
+            this.radioButton1.Location = new System.Drawing.Point(143, 63);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(94, 19);
             this.radioButton1.TabIndex = 0;
@@ -47,7 +48,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(143, 82);
+            this.radioButton2.Location = new System.Drawing.Point(143, 88);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(94, 19);
             this.radioButton2.TabIndex = 1;
@@ -66,11 +67,22 @@
             this.radioButton3.Text = "radioButton3";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CAMBIO DE VALEN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // MenuPrinciapl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
@@ -86,5 +98,6 @@
         private RadioButton radioButton1;
         private RadioButton radioButton2;
         private RadioButton radioButton3;
+        private Label label1;
     }
 }
--- MenuPrinciapl.cs
+++ MenuPrinciapl.cs
@@ -16,5 +16,10 @@ namespace grupoB_TP
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
