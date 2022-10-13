112a746912807fa2799a8f4fd65cdf220868288e
Valentina <rodriguezvalentinae@gmail.com>
2022-10-13 16:23:08 -0300

    Pantalla estado de servicio

--- EstadoDelServicio.Designer.cs
+++ EstadoDelServicio.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNumeroTrackeo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Purple;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(421, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese el Nº de trackeo para conocer el estado del servicio:";
+            // 
+            // lblNumeroTrackeo
+            // 
+            this.lblNumeroTrackeo.AutoSize = true;
+            this.lblNumeroTrackeo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNumeroTrackeo.Location = new System.Drawing.Point(158, 42);
+            this.lblNumeroTrackeo.Name = "lblNumeroTrackeo";
+            this.lblNumeroTrackeo.Size = new System.Drawing.Size(108, 21);
+            this.lblNumeroTrackeo.TabIndex = 1;
+            this.lblNumeroTrackeo.Text = "Nº de trackeo:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnBuscar.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnBuscar.Location = new System.Drawing.Point(172, 95);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(94, 26);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // EstadoDelServicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(438, 130);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblNumeroTrackeo);
+            this.Controls.Add(this.label1);
+            this.Name = "EstadoDelServicio";
             this.Text = "EstadoDelServicio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label lblNumeroTrackeo;
+        private TextBox textBox1;
+        private Button btnBuscar;
     }
 }
