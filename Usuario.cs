aedaef7f9fc485d046b056a086ab2b7f12f2e53e
FRANCISCO LEONEL GALLARINI <01GA42369886@campus.economicas.uba.ar>
2022-10-17 12:29:00 -0300

    Pantallas Solicitud Nacional

---
+++ Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grupoB_TP
+{
+    internal class Usuario
+    {
+    }
+}
