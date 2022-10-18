1433a2914f904bec533f0d11d7d4aa0710cac094
connex-clientaccess <clientaccess@connex.digital>
2022-10-18 16:13:05 -0300

    Usuario

--- Usuario.cs
+++ Usuario.cs
@@ -10,17 +10,48 @@ namespace grupoB_TP
     {
         internal static string PedirEntero(string campo, int min, int max, string valor)
         {
-            throw new NotImplementedException();
+            
+            if (int.TryParse(valor, out int result))
+            { 
+                if (result >= min && result <= max)
+                {
+                    return "";
+                }
+                else
+                {
+                    return $"El valor {campo} está fuera del rango [{min} .. {max}]. Inténtalo de nuevo.";
+                }
+            }
+            else
+            {
+                return $"{campo} No es un valor entero válido. Inténtalo de nuevo.";
+            }
         }
 
         internal static string PedirLongitudFija(string campo, int longitud, string text)
         {
-            throw new NotImplementedException();
+            if (text.Length == longitud)
+            {
+                return "";
+            }
+            else
+            {
+                return $"El valor {campo} debe tener una longitud de {longitud} caracteres. Inténtalo de nuevo.";
+            }
         }
 
         internal static string PedirVacio(string campo, string text)
         {
-            throw new NotImplementedException();
+            if(text == "")
+            {
+                return $"{campo} es un campo vacio. Inténtalo de nuevo.";
+            }
+            else
+            {
+                return "";
+            }
+            
         }
     }
 }
+
