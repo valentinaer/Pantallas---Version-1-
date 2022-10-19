3b112bc88509428aa525927642d3c0a7a8af8e83
Valentina <rodriguezvalentinae@gmail.com>
2022-10-19 12:18:16 -0300

    Pantala estado de cuenta

--- EstadoDeCuenta.cs
+++ EstadoDeCuenta.cs
@@ -19,6 +19,18 @@ namespace grupoB_TP
         private void label3_Click(object sender, EventArgs e)
         {
         }
-        
+
+        private void EstadoDeCuenta_Load(object sender, EventArgs e)
+        {
+            lblNombreCliente.Text = "------";
+            lblCuit.Text = "25-20452018-5";
+            lblFecha.Text = "14/10/2022";
+            lblNroFactura.Text = "16-0461";
+            lblEstado.Text = "Pago";
+            lblTotalFactura.Text = "$875,99";
+            lblSaldoTotal.Text = "$0,00";
+            lblFechaActual.Text = Convert.ToString(DateTime.Now);
+
+        }
     }
 }
